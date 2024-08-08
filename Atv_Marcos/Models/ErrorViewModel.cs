39588d16c6d12a25d199067992c1ca765988978b
RauanP <rauan.polli@icloud.com>
2024-08-08 17:14:01 -0300

    Commit 2

---
+++ Atv_Marcos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Atv_Marcos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
